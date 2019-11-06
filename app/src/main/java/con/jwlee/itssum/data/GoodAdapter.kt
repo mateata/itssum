572f4a1693f260c122f43ed3a1087787cc520e79
mateata <kayanepsu@gmail.com>
2019-11-06 16:55:39 +0900

    - adapter 에서 fragment 이동은 처리 못하므로, 커스텀 리스너로 외부로 연결해서 외부 Fragment에서 처리

--- app/src/main/java/con/jwlee/itssum/data/GoodAdapter.kt
+++ app/src/main/java/con/jwlee/itssum/data/GoodAdapter.kt
@@ -3,10 +3,10 @@ package con.jwlee.itssum.data
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import con.jwlee.itssum.R
 import kotlinx.android.synthetic.main.good_item.view.*
+
 
 class GoodAdapter(private val items : ArrayList<GoodData>) : RecyclerView.Adapter<GoodAdapter.ItemViewHolder>() {
 
@@ -20,10 +20,12 @@ class GoodAdapter(private val items : ArrayList<GoodData>) : RecyclerView.Adapte
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         items[position].let { data ->
             with(holder) {
-                val listener = View.OnClickListener { it ->
-                    Toast.makeText(it.context, "상세 : ${data.name}", Toast.LENGTH_SHORT).show()
+                itemView.setOnClickListener { v ->
+                    if (position != RecyclerView.NO_POSITION) {
+                        // 리스너 객체의 메서드 호출.
+                        mListener?.onItemClick(v, position, data)
+                    }
                 }
-                itemName.setOnClickListener(listener)
                 itemName.setText(data.name)
                 itemAddr.setText(data.address)
                 itemPhone.setText(data.phone)
@@ -39,5 +41,17 @@ class GoodAdapter(private val items : ArrayList<GoodData>) : RecyclerView.Adapte
         val itemName = itemView.item_name
         val itemAddr = itemView.item_address
         val itemPhone = itemView.item_phone
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(v: View, position: Int, data : GoodData)
+    }
+
+    // 리스너 객체 참조를 저장하는 변수
+    private var mListener: OnItemClickListener? = null
+
+    // OnItemClickListener 리스너 객체 참조를 어댑터에 전달하는 메서드
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.mListener = listener
     }
 }
