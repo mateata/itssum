3cbb5920afda433553ae60aae05379906db4d499
mateata <kayanepsu@gmail.com>
2019-11-13 13:57:08 +0900

    - 메인 아이콘 변경 - 불필요 요소 제거

--- app/src/main/java/con/jwlee/itssum/ui/menu/MenuFragment.kt
+++ app/src/main/java/con/jwlee/itssum/ui/menu/MenuFragment.kt
@@ -12,20 +12,13 @@ import con.jwlee.itssum.R
 
 class MenuFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: MenuViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProviders.of(this).get(MenuViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+
         return root
     }
 
