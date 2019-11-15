cb160c750a14da9cdb7917e625cad6096e4a0cc9
mateata <kayanepsu@gmail.com>
2019-11-15 11:32:04 +0900

    - 착한가게 상세뷰 임시완성

--- app/src/main/java/con/jwlee/itssum/ui/community/GoodDetailFragment.kt
+++ app/src/main/java/con/jwlee/itssum/ui/community/GoodDetailFragment.kt
@@ -8,7 +8,9 @@ import androidx.fragment.app.Fragment
 import con.jwlee.itssum.R
 import con.jwlee.itssum.data.GoodData
 import con.jwlee.itssum.util.DLog
+import kotlinx.android.synthetic.main.good_detail.*
 import kotlinx.android.synthetic.main.good_detail.view.*
+import kotlinx.android.synthetic.main.main_toolbar.*
 
 class GoodDetailFragment : Fragment() {
     override fun onCreateView(
@@ -17,14 +19,44 @@ class GoodDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.good_detail, container, false)
-        init(root)
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init(view)
     }
 
     fun init(view : View) {
         val bundle = arguments?.getBundle("detailData")
         val goodData : GoodData = bundle?.getSerializable("goodData") as GoodData
-        DLog().e(goodData.address)
-        view.text_temp.setText(goodData.address)
+        //영역 setText
+        header_title.setText(getString(R.string.title_dashboard))
+        place_sector.setText(goodData.sector)
+        place_name.setText(goodData.name)
+        place_phone.setText(goodData.phone)
+        place_intro.setText(goodData.intro)
+        if(goodData.intro == null) {
+            place_intro.visibility = View.GONE
+        }
+        place_address.setText(goodData.address)
+        place_time.setText(goodData.time)
+
+        // 주차여부 따라서 체크와 색상 변경
+        if(goodData.parking == true) {
+            place_park_yn.setImageResource(R.drawable.check_icon_yes)
+            place_park_text.setText(getString(R.string.good_detail_park_y))
+        } else {
+            place_park_yn.setImageResource(R.drawable.check_icon_no)
+            place_park_text.setText(getString(R.string.good_detail_park_n))
+        }
+        // 배달여부로 체크와 색상 변경
+        if(goodData.delivery == true) {
+            place_delivery_yn.setImageResource(R.drawable.check_icon_yes)
+            place_delivery_text.setText(getString(R.string.good_detail_delivery_y))
+        } else {
+            place_delivery_yn.setImageResource(R.drawable.check_icon_no)
+            place_delivery_text.setText(getString(R.string.good_detail_delivery_n))
+        }
     }
 }
